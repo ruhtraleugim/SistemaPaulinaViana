bba8566a59402d76d42195c305e01051e07e7fc0
Arthur Simoes <amsbsimoes@gmail.com>
2025-01-16 20:32:33 -0300

    mudanças

--- SystemPaulinaVianaMachado/SystemPaulinaVianaMachado/src/main/java/com/grupoBlueSpectre/SystemPaulinaVianaMachado/Infra/DTO/EstoqueDTO.java
+++
@@ -1,8 +0,0 @@
-package com.grupoBlueSpectre.SystemPaulinaVianaMachado.Infra.Dto;
-
-import java.time.LocalDateTime;
-
-public record EstoqueDTO(Long id,
-                         Integer quantidade,
-                         LocalDateTime dataEntrada,
-                         Long produtoId) {}
